cc98dad7be63397701a9eb7efc2d521bd9a94661
Aarim <116503935+Rengoor@users.noreply.github.com>
2024-01-22 19:15:06 +0400

    Create function that calls operators

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+// Variables
+let firstNum;
+let operator;
+let secondNum;
+
+// Operate function
+function operate(operator, num1, num2) {
+    if (operator === '+') {
+        add(num1, num2);
+    } else if (operator === '-') {
+        subtract(num1, num2);
+    } else if (operator === '*') {
+        multiply(num1, num2);
+    } else if (operator === '/') {
+        divide(num1, num2);
+    }
+}
